60d13c0c6d41074b354accda6963703264ca9907
SammeJaderson <samme.san@gmail.com>
2020-10-31 12:05:49 -0300

    implemented pages and sql formatation

--- src/main/java/com/github/sammejanderson/citiesapi/CountyResource.java
+++ src/main/java/com/github/sammejanderson/citiesapi/CountyResource.java
@@ -2,20 +2,22 @@ package com.github.sammejanderson.citiesapi;
 
 import com.github.sammejanderson.citiesapi.countries.Country;
 import com.github.sammejanderson.citiesapi.repository.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/countries")
 public class CountyResource {
 
+    @Autowired
     private CountryRepository repository;
 
     @GetMapping
-    public List<Country> countries(){
-            return repository.findAll();
-        }
+    public Page<Country> countries(Pageable page) {
+        return repository.findAll(page);
+    }
 }
